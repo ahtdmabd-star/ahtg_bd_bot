45a3562d3e26ae43a8783bfa48e78dc5d7e97f65
ahtdmabd-star <ahtdmabd@gmail.com>
2026-09-14 20:50:07 +0600

    Update mainMenu.js

--- keyboards/mainMenu.js
+++ keyboards/mainMenu.js
@@ -1,21 +1,18 @@
 const { Markup } = require('telegraf');
 
-const MINI_APP_URL = 'https://alhudatechglobal.shop/dashboard.php';
-
 function getMainMenu(lang = 'bn', isAdmin = false) {
-    let keyboard = [
-        [Markup.button.webApp('🚀 ওপেন টাস্ক অ্যাপ', MINI_APP_URL)],
-        ['📸 ইন্সটাগ্রাম কাজ', '📧 জিমেইল কাজ'],
+    const userButtons = [
+        ['📸 ইনস্টাগ্রাম কাজ', '📧 জিমেইল কাজ'],
         ['📘 ফেসবুক কাজ', '🐦 টুইটার (X) কাজ'],
         ['👤 প্রোফাইল', '💳 উইথড্র'],
-        ['📢 সাপোর্ট']
+        ['📢 অফিশিয়াল সাপোর্ট']
     ];
 
     if (isAdmin) {
-        keyboard.push(['👑 অ্যাডমিন প্যানেল']);
+        userButtons.push(['👑 অ্যাডমিন প্যানেল']);
     }
 
-    return Markup.keyboard(keyboard).resize();
+    return Markup.keyboard(userButtons).resize();
 }
 
 module.exports = { getMainMenu };
