80ae2cdf7c62fd0decb10724e30e26a34b7365ac
ahtdmabd-star <ahtdmabd@gmail.com>
2026-09-14 19:42:48 +0600

    Update mainMenu.js

--- keyboards/mainMenu.js
+++ keyboards/mainMenu.js
@@ -5,8 +5,10 @@ const MINI_APP_URL = 'https://alhudatechglobal.shop/dashboard.php';
 function getMainMenu(lang = 'bn', isAdmin = false) {
     let keyboard = [
         [Markup.button.webApp('🚀 ওপেন টাস্ক অ্যাপ', MINI_APP_URL)],
-        ['📸 ইন্সটাগ্রাম কাজ', '👤 প্রোফাইল'],
-        ['💳 উইথড্র', '📢 সাপোর্ট']
+        ['📸 ইন্সটাগ্রাম কাজ', '📧 জিমেইল কাজ'],
+        ['📘 ফেসবুক কাজ', '🐦 টুইটার (X) কাজ'],
+        ['👤 প্রোফাইল', '💳 উইথড্র'],
+        ['📢 সাপোর্ট']
     ];
 
     if (isAdmin) {
