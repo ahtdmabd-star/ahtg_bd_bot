8be46f8654f77dd2ae1fcbbbcbb5846722127611
ahtdmabd-star <ahtdmabd@gmail.com>
2026-09-14 20:19:17 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
+// MongoDB Connection String
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://alhudatechglobal_db_user:XW0TalkXq3tov5Cy@cluster0.g7zrokl.mongodb.net/alhuda_db?retryWrites=true&w=majority";
+
 const connectDB = async () => {
     try {
-        const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://alhudatechglobal_db_user:XW0TalkXq3tov5Cy@cluster0.g7zrokl.mongodb.net/?appName=Cluster0
-            ";
-        
-        await mongoose.connect(MONGO_URI, {
-            serverSelectionTimeoutMS: 5000 // ১০ সেকেন্ড অপেক্ষা না করে দ্রুত কানেক্ট করার চেষ্টা করবে
-        });
-        
+        await mongoose.connect(MONGO_URI);
         console.log('✅ MongoDB Connected Successfully!');
     } catch (err) {
         console.error('❌ MongoDB Connection Error:', err.message);
